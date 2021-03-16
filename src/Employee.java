af5f4937e5993af4036b99e88a550b742c6c5b2a
Tarcísio <tlf@ic.ufal.br>
2021-03-16 19:00:48 -0300

    Adicionado a classe employee e o cabeçalho de métodos que serão utilizados

---
+++ src/Employee.java
@@ -0,0 +1,45 @@
+
+/**
+ * Employee
+ */
+import java.util.Scanner;
+public class Employee {
+
+    String name;
+    String address;
+    String type;
+    double month_wage;
+    double two_week_wage;
+    double comission;
+
+    public static void addEmployee() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Type the name of the employee");
+        System.out.println("Type the name of the employee");
+        System.out.println("Type of employee: [0 - Hourly | 1 - Salaried | 2 - Comissioned]");
+        System.out.println("Type the payment method");
+    }
+
+    public static void removeEmployee() {
+        
+    }
+
+    public static void editEmployee() {
+        
+    }
+
+    public static void timecard() {
+        
+    }
+
+    public static void sale() {
+        
+    }
+
+    public static void feeServices() {
+        
+    }
+
+}
+
